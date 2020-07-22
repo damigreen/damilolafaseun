34ed6c579b5378d0762fc29c8177269adcfcb4ff
Damilola Faseun <fashfired@gmail.com>
2020-07-22 09:30:56 +0100

    Loaded the portfolio data from the data file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import Header from './Components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <body>
-        lasjflkajsldfkajsfkaslfja;klfjsakfjalskfjla;skjflsa;kfjlajf
-        asfkajsldfkjaldsfjalkskjflkas
-      </body>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        portfolioData: {} 
+      };
+
+      this.getData = this.getData.bind(this);
+  }
+
+  getData () {
+    axios.get('./portfolioData.json')
+      .then(response => {
+        this.setState({portfolioData: response.data})
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+  
+  render() {
+
+    return (
+      <div className="App">
+        <Header data={this.state.portfolioData} />
+      </div>
+    )
+  }
 }
 
 export default App;

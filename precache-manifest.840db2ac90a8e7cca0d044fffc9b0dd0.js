cba4c4df7bbeecc11b2f578027d69ab7f7f99bae
Damilola Faseun <fashfired@gmail.com>
2020-08-14 07:37:39 +0100

    Updates

---
+++ precache-manifest.840db2ac90a8e7cca0d044fffc9b0dd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ade6dc58da70d2f1523c9e58ecff67a",
+    "url": "/damilolafaseun/index.html"
+  },
+  {
+    "revision": "1842cbd946b4a5b44e41",
+    "url": "/damilolafaseun/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "38e623036faf1f527653",
+    "url": "/damilolafaseun/static/css/main.9903ec36.chunk.css"
+  },
+  {
+    "revision": "1842cbd946b4a5b44e41",
+    "url": "/damilolafaseun/static/js/2.177e31c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/damilolafaseun/static/js/2.177e31c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e623036faf1f527653",
+    "url": "/damilolafaseun/static/js/main.137c4971.chunk.js"
+  },
+  {
+    "revision": "e1654317707fc846c4f0",
+    "url": "/damilolafaseun/static/js/runtime-main.abf2d227.js"
+  }
+]);
